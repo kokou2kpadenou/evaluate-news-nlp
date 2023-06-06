e2bc024b47daf2cbbea49fb554438f5483ce2f0b
Kokou Kpadenou <kkokou@gmail.com>
2023-06-06 16:16:23 -0400

    chore: Minimize css and js
    NB: only in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,15 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
+  optimization: {
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+  },
   module: {
     rules: [
       {
