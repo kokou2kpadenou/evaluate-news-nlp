87f6bc65018743c6d5401056ed3ad468ec1161a0
Kokou Kpadenou <kkokou@gmail.com>
2023-06-09 00:15:37 -0400

    feat: Add route sentitment for Sentiment Analysis
    Add route sentiment for Sentiement Analysis which use MeaningCloud
    Sentiment Analysis API

--- src/server/index.js
+++ src/server/index.js
@@ -26,3 +26,48 @@ app.listen(8081, () => {
 app.get('/test', (req, res) => {
   res.send(mockAPIResponse);
 });
+
+app.get('/sentiment', async (req, res) => {
+  const { txt: txtToAnalyst } = req.query;
+
+  // In case txt is missing in query send error 400
+  if (!txtToAnalyst) {
+    res.status(400).send({ error: 'Missing parameter: txt' });
+    return;
+  }
+
+  const formdata = new FormData();
+  formdata.append('key', MC_API_CREDENTIALS);
+  formdata.append('txt', txtToAnalyst);
+  formdata.append('lang', 'en');
+
+  const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow',
+  };
+
+  try {
+    // fetch data from MeaningCloud
+    const response = await fetch('https://api.meaningcloud.com/sentiment-2.1', requestOptions);
+
+    // When status egal 200
+    if (response.status === 200) {
+      try {
+        const body = await response.json();
+        res.status(200).send(body);
+        return;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    // For any other status console log the status and throw an erreur
+    console.error(response.status);
+
+    throw new Error('Something went wrong!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
+});
