7d1e1e7618a97fff047de0e0d5e33d73bfb3ab3a
Kokou Kpadenou <kkokou@gmail.com>
2023-06-15 21:51:14 -0400

    feat: Add close/open handle functions

---
+++ src/client/js/nav.js
@@ -0,0 +1,15 @@
+/**
+ * Opens the navigation menu.
+ */
+function openNav() {
+  document.getElementById('myNav').style.width = '100%';
+}
+
+/**
+ * Closes the navigation menu.
+ */
+function closeNav() {
+  document.getElementById('myNav').style.width = '0%';
+}
+
+export { openNav, closeNav };
